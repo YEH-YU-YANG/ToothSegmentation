e43fee8b98340474af8e5c255a48abc490274314
113 - 承吉 <gemini910610@gmail.com>
2026-01-01 14:47:21 +0800

    fix split file handling

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -13,7 +13,8 @@ if os.path.exists(log_dir):
     raise FileExistsError(f'log folder "{log_dir}" already exists')
 
 os.mkdir(log_dir)
-shutil.copyfile(config.split_filename, os.path.join(log_dir, os.path.basename(config.split_filename)))
+split_filename = f'{config.split_filename}.json'
+shutil.copyfile(os.path.join('splits', split_filename), os.path.join(log_dir, os.path.basename(split_filename)))
 shutil.copyfile('configs/config.toml', os.path.join(log_dir, 'config.toml'))
 
 for fold in range(1, config.num_folds + 1):
